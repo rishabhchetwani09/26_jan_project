0b1514a8deded9d70447876c0f58cf9659d336b0
rishabhchetwani09 <123631891+rishabhchetwani09@users.noreply.github.com>
2023-04-29 13:57:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+// Change image on on Mouse Over. 
+function flipimage1() {
+    document.getElementById("image1").src = "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/160/facebook/230/flag-for-india_1f1ee-1f1f3.png";
+  }
+  
+  // Change image on on Mouse Over. 
+  function flipimage2() {
+    document.getElementById("image2").src = "https://c.ndtvimg.com/2019-01/vop8l6dg_republic-day-wishes_625x300_26_January_19.jpg";
+  }
+  // Change image on on Mouse Over. 
+  function flipimage3() {
+    document.getElementById("image3").src = "https://images.indianexpress.com/2018/01/republic-day_759_ts5.jpg";
+  }
+  // Change image on on Mouse Over. 
+  function flipimage4() {
+    document.getElementById("image4").src = "https://images.indianexpress.com/2018/01/republic-day_759_ts1.jpg";
+  }
+  // Change image on on Mouse Over. 
+  function flipimage5() {
+    document.getElementById("image5").src = "https://media4.giphy.com/media/wQEJQKTTuRoas/giphy.gif?cid=790b76119f15df782c8030915b0d180f370c45fa9612a6cb&rid=giphy.gif&ct=g";
+  }
+  // Change image on on Mouse Over. 
+  function flipimage6() {
+    document.getElementById("image6").src = "https://media3.giphy.com/media/KTWNWEhMNSs2E0yUpz/giphy.gif?cid=790b761142a742bf43866cd1054bec821442c81eb78b4066&rid=giphy.gif&ct=g";
+  }
+  // Change image on on Mouse Over. 
+  function flipimage7() {
+    document.getElementById("image7").src = "https://www.newsonline.media/wp-content/uploads/2020/01/11.jpg";
+  }
+  
+  
+  
+  // Change image on on Mouse Over. 
+  function flipimage10() {
+    document.getElementById("image10").src = "https://createcustomwishes.com/wp-content/uploads/2021/12/73rd-Republic-Day-2022-Wishes-Images-Messages-Greetings-Status-Quotes-1.jpg";
+  }
